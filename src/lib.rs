88e7e44d783b4952679657dcdab2e6ff6e03f1e6
Ilya Epifanov <elijah.epifanov@gmail.com>
2019-08-13 23:10:33 +0200

    Access to saved registers in interrupts

--- src/lib.rs
+++ src/lib.rs
@@ -196,13 +196,15 @@
 // NOTE: Adapted from cortex-m/src/lib.rs
 #![no_std]
 #![deny(missing_docs)]
-#![deny(warnings)]
 
 extern crate riscv;
 extern crate picorv32_rt_macros as macros;
 extern crate r0;
 
 pub use macros::{entry, pre_init};
+use core::ptr::NonNull;
+use core::fmt;
+use picorv32::asm;
 
 extern "C" {
     // Boundaries of the .bss section
@@ -248,27 +250,268 @@ pub unsafe extern "C" fn start_rust() -> ! {
     main();
 }
 
+/// A block of registers saved for the duration of handling an interrupt
+#[repr(C)]
+#[derive(Copy, Clone)]
+pub struct PicoRV32StoredRegisters {
+    x3: u32,
+    #[cfg(not(feature = "interrupts-qregs"))]
+    x1: u32,
+    #[cfg(not(feature = "interrupts-qregs"))]
+    x2: u32,
+    x5: u32,
+    x6: u32,
+    x7: u32,
+    x10: u32,
+    x11: u32,
+    x12: u32,
+    x13: u32,
+    x14: u32,
+    x15: u32,
+    x16: u32,
+    x17: u32,
+    x28: u32,
+    x29: u32,
+    x30: u32,
+    x31: u32,
+}
+
+impl PicoRV32StoredRegisters {
+    /// `x1`/`ra` (return address, saved by caller)
+    #[inline]
+    #[cfg(feature = "interrupts-qregs")]
+    pub fn x1(&self) -> u32 {
+        unsafe { picorv32::asm::getq2() }
+    }
+
+    /// `x1`/`ra` (return address, saved by caller)
+    #[inline]
+    #[cfg(not(feature = "interrupts-qregs"))]
+    pub fn x1(&self) -> u32 {
+        self.x1
+    }
+
+    /// `x2`/`sp` (stack pointer, saved by callee)
+    #[inline]
+    #[cfg(feature = "interrupts-qregs")]
+    pub fn x2(&self) -> u32 {
+        unsafe { picorv32::asm::getq3() }
+    }
+
+    /// `x2`/`sp` (stack pointer, saved by callee)
+    #[inline]
+    #[cfg(not(feature = "interrupts-qregs"))]
+    pub fn x2(&self) -> u32 {
+        self.x2
+    }
+
+    /// `x3`/`gp` (global pointer)
+    #[inline]
+    pub fn x3(&self) -> u32 {
+        self.x3
+    }
+
+    /// `x5`/`t0` (t0, saved by caller)
+    #[inline]
+    pub fn x5(&self) -> u32 {
+        self.x5
+    }
+
+    /// `x6`/`t1` (t1, saved by caller)
+    #[inline]
+    pub fn x6(&self) -> u32 {
+        self.x6
+    }
+
+    /// `x7`/`t2` (t2, saved by caller)
+    #[inline]
+    pub fn x7(&self) -> u32 {
+        self.x7
+    }
+
+    /// `x10`/`a0` (a0, saved by caller)
+    #[inline]
+    #[cfg(not(feature = "interrupts-qregs"))]
+    pub fn x10(&self) -> u32 {
+        self.x10
+    }
+
+    /// `x11`/`a1` (a1, saved by caller)
+    #[inline]
+    #[cfg(not(feature = "interrupts-qregs"))]
+    pub fn x11(&self) -> u32 {
+        self.x11
+    }
+
+    /// `x12`/`a2` (a2, saved by caller)
+    #[inline]
+    #[cfg(not(feature = "interrupts-qregs"))]
+    pub fn x12(&self) -> u32 {
+        self.x12
+    }
+
+    /// `x13`/`a3` (a3, saved by caller)
+    #[inline]
+    pub fn x13(&self) -> u32 {
+        self.x13
+    }
+
+    /// `x14`/`a4` (a4, saved by caller)
+    #[inline]
+    pub fn x14(&self) -> u32 {
+        self.x14
+    }
+
+    /// `x15`/`a5` (a5, saved by caller)
+    #[inline]
+    pub fn x15(&self) -> u32 {
+        self.x15
+    }
+
+    /// `x16`/`a6` (a6, saved by caller)
+    #[inline]
+    pub fn x16(&self) -> u32 {
+        self.x16
+    }
+
+    /// `x17`/`a7` (a7, saved by caller)
+    #[inline]
+    pub fn x17(&self) -> u32 {
+        self.x17
+    }
+
+    /// `x28`/`t3` (t3, saved by caller)
+    #[inline]
+    pub fn x28(&self) -> u32 {
+        self.x28
+    }
+
+    /// `x29`/`t4` (t4, saved by caller)
+    #[inline]
+    pub fn x29(&self) -> u32 {
+        self.x29
+    }
+
+    /// `x30`/`t5` (t5, saved by caller)
+    #[inline]
+    pub fn x30(&self) -> u32 {
+        self.x30
+    }
+
+    /// `x31`/`t6` (t6, saved by caller)
+    #[inline]
+    pub fn x31(&self) -> u32 {
+        self.x31
+    }
+}
+
+impl fmt::Debug for PicoRV32StoredRegisters {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let pc = if self.x1() & 1 == 1 {
+            self.x1() - 3
+        } else {
+            self.x1() - 4
+        };
+
+        let (instr, long_instr) = {
+            let mut instr: u32 = *(unsafe { NonNull::new_unchecked(pc as *mut u16).as_ref() }) as u32;
+            let long_instr = (instr & 3) == 3;
+            if long_instr {
+                let instr2 = *(unsafe { NonNull::new_unchecked((pc + 2) as *mut u16).as_ref() }) as u32;
+                instr = instr | instr2 << 16;
+            }
+            (instr, long_instr)
+        };
+
+        write!(f, "RA: {:08x}\tINSTR: ", self.x1())?;
+        if long_instr {
+            writeln!(f, "{:08x}", instr)?;
+        } else {
+            writeln!(f, "{:04x}", instr)?;
+        }
+
+        writeln!(f, "SP: {:08x}\tGP: {:08x}", self.x2(), self. x3())?;
+        writeln!(f, "T0: {:08x}\tT1: {:08x}\tT2: {:08x}", self.x5(), self.x6(), self.x7())?;
+        writeln!(f, "A0: {:08x}\tA1: {:08x}\tA2: {:08x}\tA3: {:08x}", self.x10(), self.x11(), self.x12(), self.x13())?;
+        writeln!(f, "A4: {:08x}\tA5: {:08x}\tA6: {:08x}\tA7: {:08x}", self.x14(), self.x15(), self.x16(), self.x17())?;
+        writeln!(f, "T3: {:08x}\tT4: {:08x}\tT5: {:08x}\tT6: {:08x}", self.x28(), self.x29(), self.x30(), self.x31())?;
+        Ok(())
+    }
+}
+
+/// All stored registers
+#[repr(C)]
+#[derive(Copy, Clone)]
+pub struct PicoRV32AllStoredRegisters {
+    x3: u32,
+    x1: u32,
+    x2: u32,
+    x5: u32,
+    x6: u32,
+    x7: u32,
+    x10: u32,
+    x11: u32,
+    x12: u32,
+    x13: u32,
+    x14: u32,
+    x15: u32,
+    x16: u32,
+    x17: u32,
+    x28: u32,
+    x29: u32,
+    x30: u32,
+    x31: u32,
+}
+
+impl From<PicoRV32StoredRegisters> for PicoRV32AllStoredRegisters {
+    fn from(r: PicoRV32StoredRegisters) -> Self {
+        if cfg!(feature = "interrupts-qregs") {
+            PicoRV32AllStoredRegisters {
+                x3: r.x3(),
+                x1: r.x1(),
+                x2: r.x2(),
+                x5: r.x5(),
+                x6: r.x6(),
+                x7: r.x7(),
+                x10: r.x10(),
+                x11: r.x11(),
+                x12: r.x12(),
+                x13: r.x13(),
+                x14: r.x14(),
+                x15: r.x15(),
+                x16: r.x16(),
+                x17: r.x17(),
+                x28: r.x28(),
+                x29: r.x29(),
+                x30: r.x30(),
+                x31: r.x31(),
+            }
+        } else {
+            unsafe { core::mem::transmute_copy(&r) }
+        }
+    }
+}
 
 /// Trap entry point rust (_start_trap_rust)
 ///
 /// `irqs` is a bitmask off IRQs to handle
 #[link_section = ".trap.rust"]
 #[export_name = "_start_trap_rust"]
-pub extern "C" fn start_trap_rust(irqs: u32) {
+pub extern "C" fn start_trap_rust(regs: *const u32, irqs: u32) {
     extern "C" {
-        fn trap_handler(irqs: u32);
+        fn trap_handler(regs: &PicoRV32StoredRegisters, irqs: u32);
     }
 
     unsafe {
         // dispatch trap to handler
-        trap_handler(irqs);
+        trap_handler(NonNull::new_unchecked(regs as *mut PicoRV32StoredRegisters).as_ref(), irqs);
     }
 }
 
 
 /// Default Trap Handler
 #[no_mangle]
-pub fn default_trap_handler(_irqs: u32) {}
+pub extern "Rust" fn default_trap_handler(_irqs: u32) {}
 
 #[doc(hidden)]
 #[no_mangle]
@@ -280,27 +523,27 @@ pub unsafe extern "Rust" fn default_pre_init() {}
 /// use core::sync::atomic;
 /// use core::sync::atomic::Ordering;
 ///
-/// pub fn timer() {
+/// pub fn timer(_regs: &picorv32_rt::PicoRV32StoredRegisters) {
 ///     // ...
 /// }
 ///
-/// pub fn illegal_instruction() {
+/// pub fn illegal_instruction(_regs: &picorv32_rt::PicoRV32StoredRegisters) {
 ///     loop {
 ///         atomic::compiler_fence(Ordering::SeqCst);
 ///     }
 /// }
 ///
-/// pub fn bus_error() {
+/// pub fn bus_error(_regs: &picorv32_rt::PicoRV32StoredRegisters) {
 ///     loop {
 ///         atomic::compiler_fence(Ordering::SeqCst);
 ///     }
 /// }
 ///
-/// pub fn irq5() {
+/// pub fn irq5(_regs: &picorv32_rt::PicoRV32StoredRegisters) {
 ///     // ...
 /// }
 ///
-/// pub fn irq6() {
+/// pub fn irq6(_regs: &picorv32_rt::PicoRV32StoredRegisters) {
 ///     // ...
 /// }
 ///
@@ -314,17 +557,23 @@ pub unsafe extern "Rust" fn default_pre_init() {}
 /// ```
 #[cfg(feature = "interrupts")]
 #[macro_export] macro_rules! picorv32_interrupts {
-    (@interrupt ($n:literal, $pending_irqs:expr, $handler:ident)) => {
+    (@interrupt ($n:literal, $pending_irqs:expr, $regs:expr, $handler:ident)) => {
         if $pending_irqs & (1 << $n) != 0 {
-            $handler();
+            $handler($regs);
         }
     };
     ( $( $irq:literal : $handler:ident ),* ) => {
         #[no_mangle]
-        pub extern "C" fn trap_handler(pending_irqs: u32) {
+        pub extern "C" fn trap_handler(regs: *const picorv32_rt::PicoRV32StoredRegisters, pending_irqs: u32) {
+            let regs = unsafe { regs.as_ref().unwrap() };
             $(
-                picorv32_interrupts!(@interrupt($irq, pending_irqs, $handler));
+                picorv32_interrupts!(@interrupt($irq, pending_irqs, regs, $handler));
             )*
         }
     };
+}
+
+/// sleep until an interrupt is received
+pub fn wfi() {
+    let _irqs = unsafe { asm::waitirq() };
 }
