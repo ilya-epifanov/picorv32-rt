16214f8c5d18ccab8f12f622af1b5494f50c2eb6
Toby Gomersall <toby.gomersall@smartacoustics.co.uk>
2022-04-14 14:38:25 +0100

    [ENH] Removed the last of the inline-asm feature.

--- ci/script.sh
+++ ci/script.sh
@@ -2,10 +2,6 @@ set -euxo pipefail
 
 main() {
     cargo check --target $TARGET --features "$FEATURES"
-
-    if [ $TRAVIS_RUST_VERSION = nightly ]; then
-        cargo check --target $TARGET --features "inline-asm,$FEATURES"
-    fi
 }
 
 main
